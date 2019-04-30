8129273fbcd45c333db76675b3c8b3526334d50e
BelleChou <belle414489778@qq.com>
2019-04-30 15:31:48 +0800

    神奇的koa-routerbug

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
 const Koa = require('koa');
-var routers = require('./server/routes/index');
-const json = require('koa-json');
-const logger = require('koa-logger');
- 
+const routers = require('./server/routes/index');
 const app=new Koa()
-
 app.use(require('koa-bodyparser')());
-app.use(json());
-app.use(logger());
 
 // 对于任何请求，app将调用该异步函数处理请求：
 app.use(async (ctx,next) => {
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,10 @@
 const ThemeController = require('../controllers/theme')
-const router = require('koa-router')()
+const router = require('koa-router')();
 
-//const router = new Router();
-
-// router.get('/',(ctx)=>{
-//     ctx.body="welcome"
-// });
+router.get('/',(ctx)=>{
+    ctx.body="welcome"
+});
 
 router.get('/theme',ThemeController.allTheme);
+
+module.exports = router
