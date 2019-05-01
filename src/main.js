eff3fc29a339ecde1adf86c745cb969e6a28d506
BelleChou <belle414489778@qq.com>
2019-05-01 13:53:24 +0800

    hhhh

--- src/main.js
+++ src/main.js
@@ -16,7 +16,13 @@ import 'echarts-wordcloud/dist/echarts-wordcloud'
 import 'echarts-wordcloud/dist/echarts-wordcloud.min'
 
 window.axios = require('axios');
-window.axios.defaults.baseURL = 'http://127.0.0.1:3000'
+window.axios.defaults.baseURL= 'static';
+
+if (/http:\/\/localhost|http:\/\/127.0.0.1/.test(location.href)) {
+  window.axios.defaults.baseURL = 'http://127.0.0.1:3000'
+}
+
+console.log(window.axios.defaults.baseURL);
 
 new Vue({
   el: '#app',
